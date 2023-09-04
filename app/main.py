4cc064d413684df36a303950afa65368a4160ef8
v1-k <vinitpce@gmail.com>
2023-09-03 23:14:12 -0500

    updating cors

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ from .db.session import engine
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-origins = ["http://stackapex.com", "https://stackapex.com"]
+# origins = ["http://stackapex.com", "https://stackapex.com"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE"],
+# )
 
 app.include_router(public.router)
