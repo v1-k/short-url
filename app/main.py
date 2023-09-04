e0e0644c8a09d50c3bcd258a4b37fd6ed18f48c1
v1-k <vinitpce@gmail.com>
2023-09-03 21:35:30 -0500

    adding cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .router import public
 from .db import model
 from .db.session import engine
 
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["http://stackapex.com", "https://stackapex.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+)
+
 app.include_router(public.router)
